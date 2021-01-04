52304fa8c385d30da7b9861ba728858a8cd73b39
Matt Werth <matthewwerth@gmail.com>
2021-01-04 08:43:11 -0500

    Added Custom Compare Operations

--- src/com/werth/CompareOperations.java
+++ src/com/werth/CompareOperations.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CompareOperations {
     public static void main(String[] args) {
@@ -27,15 +28,29 @@ public class CompareOperations {
         ZComparablePerson matt = new ZComparablePerson("Matt", 33, 195.0);
         ZComparablePerson ally = new ZComparablePerson("Ally", 25, 136.1);
         ZComparablePerson amanda = new ZComparablePerson("Amanda", 31, 144.3);
+        ZComparablePerson charles = new ZComparablePerson("Charles", 45, 212.8);
 
         List<ZComparablePerson> people = new ArrayList<>();
         people.add(matt);
         people.add(amanda);
         people.add(ally);
+        people.add(charles);
 
         people.stream()
                 .sorted((per1, per2) -> per1.compareTo(per2))
                 .forEach(per -> System.out.println(per.getName()));
 
+        System.out.println(" --- --- --- ");
+
+        // TODO: 1/4/21 We could also do a Comparator here...
+        // This is another easy way to do a custom compare. Just pass in type and a lambda and you can sort!
+
+        Comparator<ZComparablePerson> perCompare = (per1, per2) -> per1.getAge() - per2.getAge();
+
+        List<ZComparablePerson> ageSort = (List<ZComparablePerson>) people.stream()
+                .sorted(perCompare)
+                .collect(Collectors.toList());
+
+        ageSort.forEach(per -> System.out.println(per.getName()));
     }
 }
